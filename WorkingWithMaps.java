83973c62b4ddb1f1ebf9855472069fd7213e9629
alexngUNC <ageorgiev@unc.edu>
2022-05-31 15:59:37 -0400

    Finished amigoscode vid

--- WorkingWithMaps.java
+++ WorkingWithMaps.java
@@ -1,8 +1,19 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class WorkingWithMaps {
     public static void main(String[] args) {
+        Map<Person, Diamond> map = new HashMap<>();
+        map.put(new Person("Jamila"), new Diamond("African"));
+
+        System.out.println(new Person("Jamila").hashCode());
+        System.out.println(new Person("Jamila").hashCode());
+
+        System.out.println(map.get(new Person("Jamila")));
+    }
+
+    private static void maps() {
         Map<Integer, Person> map = new HashMap<>();
         map.put(1, new Person("Alex"));
         map.put(2, new Person("Alexa"));
@@ -27,5 +38,33 @@ public class WorkingWithMaps {
         System.out.println(map.values());
     }
 
-    record Person(String name){}
+    static class Person {
+        String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override // Always override equals and hashcode unless you use a record,
+        public String toString() { // same hash code has to return for any given object
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+
+    record Diamond(String name){}
 }
